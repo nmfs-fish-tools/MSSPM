fdf193ffee3e6021eaa34065f56566d2bc4830b7
Ron <ronald.klasky@noaa.gov>
2022-01-03 15:28:07 -0500

    Mods for v0.9.45

--- MSSPM_GuiCovariate/nmfCovariateTab02.cpp
+++
@@ -1,8 +0,0 @@
-#include "nmfCovariateTab02.h"
-
-nmfCovariate_Tab2::nmfCovariate_Tab2(QTabWidget*  tabs,
-                                     nmfLogger*   logger,
-                                     nmfDatabase* databasePtr,
-                                     std::string& projectDir)
-{
-}
